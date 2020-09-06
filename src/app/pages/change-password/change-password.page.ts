4b8a899ce56d2bc115972e349f2c1a4c1e227d87
ravi-ashara <raviashara94@gmail.com>
2020-09-06 13:45:04 +0530

    Design change password page

--- src/app/pages/change-password/change-password.page.ts
+++ src/app/pages/change-password/change-password.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.page.html',
   styleUrls: ['./change-password.page.scss'],
 })
-export class ChangePasswordPage implements OnInit {
+export class ChangePasswordPage {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  changePassword() {
 
+  }
 }
