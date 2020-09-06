0b527dc255a4fcf3ede3ce930044593aabf0550d
ravi-ashara <raviashara94@gmail.com>
2020-09-06 13:43:46 +0530

    Set Hardware BackButton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { Platform, NavController } from '@ionic/angular';
+import { Platform, NavController, IonRouterOutlet } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { CommonServiceService } from './services/common-service/common-service.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-
+  @ViewChild(IonRouterOutlet, { static: false }) routerOutlet: IonRouterOutlet;
   public appPages = [
     {
       title: 'Home',
@@ -43,14 +45,17 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private navControl: NavController
+    private navControl: NavController,
+    public router: Router,
+    public commonService: CommonServiceService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.handleHardwareBackButton();
+      this.statusBar.styleLightContent();
       setTimeout(() => {
         this.splashScreen.hide();
       }, 5000);
@@ -60,5 +65,23 @@ export class AppComponent {
   logoutFunction() {
     localStorage.removeItem('isLogin');
     this.navControl.navigateRoot('/login')
+  }
+
+  handleHardwareBackButton() {
+    this.platform.backButton.subscribe((event: any) => {
+      if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+        this.routerOutlet.pop();
+      } else if (this.router.url === '/login') {
+        navigator['app'].exitApp();
+      } else {
+        if (this.router.url === '/home') {
+          this.commonService.showConfirm('Fast Food', 'Do you wish to exit?', ['Cancel', 'Exit'], (res: any) => {
+            if (res === "Yes") {
+              navigator['app'].exitApp();
+            }
+          });
+        }
+      }
+    });
   }
 }
